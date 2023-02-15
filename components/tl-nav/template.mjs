38ddbd071e5a64c77e6f62499951caad6af82844
Thon Ly <thonly@heartbank.ceo>
2023-02-15 07:54:10 -0800

    reorder

--- components/tl-nav/template.mjs
+++ components/tl-nav/template.mjs
@@ -3,11 +3,13 @@ const template = document.createElement("template");
 template.innerHTML = `
     <link rel="stylesheet" href="components/tl-nav/shadow.css">
     <nav>
+        <h3>Sitemap</h3>
         <ul>
-            <li><a href="monthly-earnings">My Monthly Earnings</a></li>
-            <li><a href="check-in-instructions">My Check-In Instructions</a></li>
-            <li><a href="scheduled-messages">My Scheduled Messages</a></li>
-            <li><a href="house-manual">My House Manual</a></li>
+            <li><a href="/">Airbnb Superhost Consultant</a></li>
+            <li><a href="/monthly-earnings">My Monthly Earnings</a></li>
+            <li><a href="/check-in-instructions">My Check-In Instructions</a></li>
+            <li><a href="/scheduled-messages">My Scheduled Messages</a></li>
+            <li><a href="/house-manual">My House Manual</a></li>
         </ul>
     </nav>
 `;
