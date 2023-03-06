c9405cf6431f848dccd47ddee3a83e7e8f412611
Thon Ly <thonly@heartbank.ceo>
2023-03-06 06:34:43 -0800

    refactor

--- components/tl-footer/template.mjs
+++ components/tl-footer/template.mjs
@@ -2,7 +2,7 @@ const template = document.createElement("template");
 
 template.innerHTML = `
     <link rel="stylesheet" href="/components/tl-footer/shadow.css">
-    <h6><a href="https://thonly.org">Thon Ly</a> &copy; 2023</h6>
+    <h6><a href="https://siliconwat.dev">Silicon Wat LLC</a> &copy; 2023</h6>
 `;
 
 export default template;
