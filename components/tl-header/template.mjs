a80f6662b23e28d3722a9a630ba5bfd25251afad
Thon Ly <thonly@heartbank.ceo>
2023-03-16 10:21:46 -0700

    begin v2

--- components/tl-header/template.mjs
+++ components/tl-header/template.mjs
@@ -4,9 +4,10 @@ template.innerHTML = `
     <link rel="stylesheet" href="/components/tl-header/shadow.css">
     <ul>
         <li><a href="/">Home</a></li>
-        <li><a href="/superhost-consultant">Airbnb Superhost Consultant</a></li>
-        <li><a href="/monthly-earnings">My Monthly Earnings</a></li>
+        <li><a href="/superhost-ambassador">Airbnb Superhost Ambassador</a></li>
+        <li><a href="/price-stats">Airbnb Price Stats</a></li>
         <li><a href="/check-in-instructions">My Check-In Instructions</a></li>
+        <li><a href="/monthly-earnings">My Monthly Earnings</a></li>
         <li><a href="/scheduled-messages">My Scheduled Messages</a></li>
         <li><a href="/house-manual">My House Manual</a></li>
     </ul>
@@ -14,7 +15,8 @@ template.innerHTML = `
         <h3 onclick="this.getRootNode().host.menu()">Menu</h3> 
         <select onchange="this.getRootNode().host.page(this)">
             <option value="/">Home</option>
-            <option value="/superhost-consultant/">Airbnb Superhost Consultant</option>
+            <option value="/superhost-ambassador/">Airbnb Superhost Ambassador</option>
+            <option value="/price-stats/">Airbnb Price Stats</option>
             <option value="/monthly-earnings/">My Monthly Earnings</option>
             <option value="/check-in-instructions/">My Check-In Instructions</option>
             <option value="/scheduled-messages/">My Scheduled Messages</option>
