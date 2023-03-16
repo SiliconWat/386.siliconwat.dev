cc3bc8e389e87647f21d1e62986eae2a0fb08d2d
Thon Ly <thonly@heartbank.ceo>
2023-03-16 16:53:12 -0700

    refactor

--- components/tl-header/template.mjs
+++ components/tl-header/template.mjs
@@ -6,9 +6,9 @@ template.innerHTML = `
         <li><a href="/">Home</a></li>
         <li><a href="/superhost-ambassador">Airbnb Superhost Ambassador</a></li>
         <li><a href="/price-stats">Airbnb Price Stats</a></li>
-        <li><a href="/check-in-instructions">My Check-In Instructions</a></li>
         <li><a href="/monthly-earnings">My Monthly Earnings</a></li>
         <li><a href="/scheduled-messages">My Scheduled Messages</a></li>
+        <li><a href="/check-in-instructions">My Check-In Instructions</a></li>
         <li><a href="/house-manual">My House Manual</a></li>
     </ul>
     <nav>
@@ -18,8 +18,8 @@ template.innerHTML = `
             <option value="/superhost-ambassador/">Airbnb Superhost Ambassador</option>
             <option value="/price-stats/">Airbnb Price Stats</option>
             <option value="/monthly-earnings/">My Monthly Earnings</option>
-            <option value="/check-in-instructions/">My Check-In Instructions</option>
             <option value="/scheduled-messages/">My Scheduled Messages</option>
+            <option value="/check-in-instructions/">My Check-In Instructions</option>
             <option value="/house-manual/">My House Manual</option>
         </select>
         <!--<button onclick="window.location.reload()">Refresh</button>-->
