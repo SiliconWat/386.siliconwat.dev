04133ff58081e667762cd55c4415a26b9cbffc8a
Thon Ly <thonly@heartbank.ceo>
2023-03-29 12:40:40 -0700

    begin competitors

---
+++ competitors/index.js
@@ -0,0 +1,6 @@
+const URL = "https://www.airbnb.com/s/Fresno--California--United-States/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&price_filter_input_type=0&price_filter_num_nights=2&query=Fresno%2C%20CA&place_id=ChIJnU6eVOFdlIAREbijSWRAEns&date_picker_type=flexible_dates&adults=1&source=structured_search_input_header&search_type=filter_change&flexible_trip_lengths%5B%5D=weekend_trip&channel=EXPLORE&flexible_trip_dates%5B%5D=april"
+"https://www.furnishedfinder.com/stats?ci=Fresno&st=California"
+
+function updateUrl(event) {
+
+}
