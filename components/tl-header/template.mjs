b52dbeb0333b0fe51141be42f2499efdb29c027c
Thon Ly <thonly@siliconwat.dev>
2023-09-08 15:43:39 -0700

    v2 finish

--- components/tl-header/template.mjs
+++ components/tl-header/template.mjs
@@ -4,25 +4,21 @@ template.innerHTML = `
     <link rel="stylesheet" href="/components/tl-header/shadow.css">
     <ul>
         <li><a href="/">Home</a></li>
-        <li><a href="/superhost-ambassador">Airbnb Superhost Ambassador</a></li>
         <li><a href="/monthly-earnings">My Monthly Earnings</a></li>
         <li><a href="/scheduled-messages">My Scheduled Messages</a></li>
         <li><a href="/check-in-instructions">My Check-In Instructions</a></li>
         <li><a href="/house-manual">My House Manual</a></li>
-        <li><a href="/competitors">Competitors</a></li>
-        <li><a href="/housekeeping">Housekeeping</a></li>
+        <li><a href="/housekeeping">My Housekeeper Tasks</a></li>
     </ul>
     <nav>
         <h3 onclick="this.getRootNode().host.menu()">Menu</h3> 
         <select onchange="this.getRootNode().host.page(this)">
             <option value="/">Home</option>
-            <option value="/superhost-ambassador/">Airbnb Superhost Ambassador</option>
             <option value="/monthly-earnings/">My Monthly Earnings</option>
             <option value="/scheduled-messages/">My Scheduled Messages</option>
             <option value="/check-in-instructions/">My Check-In Instructions</option>
             <option value="/house-manual/">My House Manual</option>
-            <option value="/competitors/">Competitors</option>
-            <option value="/housekeeping/">Housekeeping</option>
+            <option value="/housekeeping/">My Housekeeper Tasks</option>
         </select>
         <!--<button onclick="window.location.reload()">Refresh</button>-->
     </nav>
